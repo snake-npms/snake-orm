f4a2ba4975d58e1e0dbeb0b5061026a1de6ba89e
mars <mars@mars.com>
2018-09-27 18:28:48 +0800

    add (count & sum)

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ const User = require('./models/User')
 new SnakeOrm('database_development', 'root', null, {dialect: 'mysql', host: 'localhost'})
 
 async function test() {
-	let result = await User.where({username: 'create'}).order([{username: 'desc'}, 'age']).limit(1).offset(1)
-	// console.log(result)
+	let result = await User.where({username: 'create'}).order([{username: 'desc'}, 'age']).sum('age')
+	console.log(result)
 }
 test()
