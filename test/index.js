76164d389491408b9040d63cc66526c991be3db8
mars <mars@mars.com>
2018-09-28 10:27:37 +0800

    sqlite3 support run sql

--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
 const SnakeOrm = require('../index')
 const User = require('./models/User')
-new SnakeOrm('database_development', 'root', null, {dialect: 'mysql', host: 'localhost'})
+// new SnakeOrm('database_development', 'root', null, {dialect: 'mysql', host: 'localhost'})
+new SnakeOrm('database_development.sqlite3', 'root', null, {dialect: 'sqlite3', host: 'localhost'})
 
 async function test() {
+	await SnakeOrm.sharedSnakeOrmProxy().runSql(`CREATE TABLE IF NOT EXISTS users (username VARCHAR(20), age INTEGER)`)
 	let result = await User.where({username: 'create'}).order([{username: 'desc'}, 'age']).sum('age')
 	console.log(result)
 }
