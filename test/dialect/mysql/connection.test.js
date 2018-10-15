9494a8a556416eb139913754b8abb52f80f1427a
mars <mars@mars.com>
2018-10-15 21:49:11 +0800

    base support

--- test/dialect/mysql/connection.test.js
+++ test/dialect/mysql/connection.test.js
@@ -1,16 +1,17 @@
 const path = require('path')
 const SnakeOrm = require('../../../index')
 const expect = require('chai').expect
-let snakeOrmProxy = SnakeOrm.getOrCreateSnakeOrmProxy('database_test', 'root', null, {dialect: 'mysql', host: 'localhost', logger: true})
+// let snakeOrmProxy = SnakeOrm.getOrCreateSnakeOrmProxy('database_test', 'root', null, {dialect: 'mysql', host: 'localhost', logger: true})
 describe('Mysql connect TEST', function() {
 	before(async function() {
-		await snakeOrmProxy.registerModelsWithPath(path.resolve(__dirname, './support/models'))
+		// await snakeOrmProxy.registerModelsWithPath(path.resolve(__dirname, './support/models'))
+		let snakeOrmProxy = await SnakeOrm.initWithSnakeOrmRcFile(path.resolve(__dirname, 'support/.snakeormrc'))
 		// 在本区块的所有测试用例之前执行
 		// both snakeOrm.proxy.runSql and snakeOrm.dialect.runSql are ok!
 		await snakeOrmProxy.runSql(`DROP TABLE IF EXISTS users`)
 		await snakeOrmProxy.runSql(`DROP TABLE IF EXISTS wallets`)
 		await snakeOrmProxy.runSql(`DROP TABLE IF EXISTS orders`)
-		await snakeOrmProxy.runSql(`DROP TABLE IF EXISTS order_items`)
+		await snakeOrmProxy.runSql(`DROP TABLE IF EXISTS orderItems`)
 		await snakeOrmProxy.runSql(`CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTO_INCREMENT, username VARCHAR(20), age INTEGER)`)
 		await snakeOrmProxy.runSql(`CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY AUTO_INCREMENT, oid VARCHAR(20), name VARCHAR(20), userId INTEGER)`)
 		await snakeOrmProxy.runSql(`CREATE TABLE IF NOT EXISTS orderItems (id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), orderId INTEGER)`)
@@ -28,12 +29,13 @@ describe('Mysql connect TEST', function() {
 	});
 	
 	it('should connect success', function() {
+		let snakeOrmProxy = SnakeOrm.getOrmProxyByDatabase('database_test')
 		let connectOptions = snakeOrmProxy.proxy.connectOptions
 		expect(snakeOrmProxy.proxy).to.be.equal(snakeOrmProxy);
 		expect(connectOptions).to.be.an('object');
 		expect(connectOptions.dialect).to.be.equal('mysql');
 		expect(snakeOrmProxy).to.be.equal(SnakeOrm.getOrmProxyByDatabase(connectOptions.database));
+		
+		require('../association')(snakeOrmProxy)
 	});
-	
-	require('../association')(snakeOrmProxy)
 })
---
+++ test/dialect/sqlite3/support/db/migrations/20181015034750-create-roles.js
@@ -0,0 +1,14 @@
+module.exports = {
+	async up (ormProxy) {
+		// await ormProxy.runSql(`CREATE TABLE IF NOT EXISTS admins (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(20), age INTEGER)`)
+		await ormProxy.createTable('roles', {}, function (t) {
+			t.string('name')
+			t.string('desc')
+			t.references('admin', {foreignKey: true})
+			t.boolean('enabled', {default: false})
+			t.timestamps()
+		})
+		await ormProxy.addIndex('roles', 'desc')
+		await ormProxy.addIndex('roles', ['name', 'desc'])
+	}
+}
