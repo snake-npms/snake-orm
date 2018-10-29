284647eddda0c36acf7fc2b67150cf415523748a
mars <mars@mars.com>
2018-10-29 17:55:42 +0800

    support (beforeSave & beforeCreate & afterCreate & beforeUpdate & afterUpdate & afterSave)

--- test/dialect/sqlite3/support/models/User.js
+++ test/dialect/sqlite3/support/models/User.js
@@ -15,10 +15,37 @@ class User extends SnakeModel {
 		this.hasMany('orderItems', {through: 'orders'})
     this.hasMany('friendShips')
     this.hasMany('friends', {through: 'friendShips', className: 'User', foreignKey: 'friendId'})
+		
+    // this.beforeSave = ['testBeforeSave']
+    // this.beforeCreate = ['testBeforeCreate']
+    // this.afterCreate = ['testAfterCreate']
+    // this.beforeUpdate = ['testBeforeUpdate']
+    // this.afterUpdate = ['testAfterUpdate']
+    // this.afterSave = ['testAfterSave']
 	}
 	
 	constructor (values) {
 		super(...arguments)
 	}
+  
+  testBeforeSave () {
+		console.log('==========================testBeforeSave')
+  }
+  testBeforeCreate () {
+    console.log('==========================testBeforeCreate')
+  }
+  
+  testAfterCreate () {
+    console.log('==========================testAfterCreate')
+  }
+  testBeforeUpdate () {
+    console.log('==========================testBeforeUpdate')
+  }
+  testAfterUpdate () {
+    console.log('==========================testAfterUpdate')
+  }
+  testAfterSave () {
+    console.log('==========================testAfterSave')
+  }
 }
 module.exports = User
